fd94c06263e7278221f6b2a49dfcf67f08a63063
Alexandre <alex.nakano@gmail.com>
2019-06-09 09:53:15 -0300

    Fornecimento local para arquivos vendor.

--- lib/php/header.php
+++ lib/php/header.php
@@ -12,30 +12,30 @@
     <link rel="icon" href="img/favicon.ico">
 
     <!-- jquery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="vendor/jquery.min.js"></script>
 
     <!-- canvasjs -->
-    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <script src="vendor/canvasjs.min.js"></script>
 
     <!-- Reseta todo CSS -->
     <link rel="stylesheet" type="text/css" href="lib/css/reset.css">
 
     <!-- Normalizar CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" crossorigin="anonymous">    
+    <link rel="stylesheet" href="vendor/normalize.min.css">    
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="vendor/bootstrap.min.css">
 
     <!-- popper js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="vendor/popper.min.js"></script>
 
     <!-- bootstrap js -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="vendor/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/jquery.dataTables.min.css">
     
 
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="vendor/jquery.dataTables.min.js"></script>
 	<title>Jogo da forca</title>
 
     <!-- CSS de estilo de elementos gerais da tela do jogo -->
